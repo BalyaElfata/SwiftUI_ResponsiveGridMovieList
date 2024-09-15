568398ad13e62a0e308ae9a7945cf2da1490d2af
Balya Elfata <balyaelfata@yahoo.com>
2024-09-15 21:03:38 +0700

    Initial Commit

---
+++ SwiftUI_ResponsiveGridMovieList/SwiftUI_ResponsiveGridMovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ResponsiveGridMovieListApp.swift
+//  SwiftUI_ResponsiveGridMovieList
+//
+//  Created by Balya Elfata on 15/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ResponsiveGridMovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
