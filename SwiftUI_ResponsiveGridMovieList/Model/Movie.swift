8d5c6afde3bc6085690989035de74b9a0b80e297
Balya Elfata <balyaelfata@yahoo.com>
2024-09-16 00:18:02 +0700

    initial Movie model

--- SwiftUI_ResponsiveGridMovieList/Model/Movie.swift
+++ SwiftUI_ResponsiveGridMovieList/Model/Movie.swift
@@ -1,3 +1,12 @@
-class Movie: Identifiable, Codable {
-    let id = UUID().uuidString
+import Foundation
+
+struct Movie: Identifiable, Codable {
+    var id = UUID().uuidString
+    let originalTitle: String
+    let releaseDate: String
+    
+    enum CodingKeys : String, CodingKey {
+        case originalTitle = "original_title"
+        case releaseDate = "release_date"
+    }
 }
