9baf4b9ec4f3eb4a28363f1df539bb04509e06f2
Balya Elfata <balyaelfata@yahoo.com>
2024-09-16 00:18:19 +0700

    view testing using color

--- SwiftUI_ResponsiveGridMovieList/ContentView.swift
+++ SwiftUI_ResponsiveGridMovieList/ContentView.swift
@@ -1,17 +1,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    let data = Array(1...50)
+    
+    var columns = Array(repeating: GridItem(.flexible(), spacing: 1), count: 4)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { proxy in
+            ScrollView {
+                let itemWidth = proxy.size.width / 4 - 4
+                let itemHeight = proxy.size.height / 6
+                LazyVGrid(columns: self.columns, spacing: 4) {
+                    ForEach(data, id: \.self) { number in
+                        Color.blue
+                            .frame(width: itemWidth, height: itemHeight)
+                    }
+                }
+            }
         }
         .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
